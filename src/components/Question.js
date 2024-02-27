44d53dbbd7dda4fcaf5787b810319adb4865ea20
elmar <elmarcera@gmail.com>
2024-02-27 15:10:25 +0800

    Moving to the Next Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,9 @@
 import Options from "./Options";
 
-function Question({ question, dispatch, answer, points }) {
+function Question({ question, dispatch, answer }) {
   console.log(question);
   return (
     <div>
-      <h2>{points}</h2>
       <h4>{question.question}</h4>
       <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
